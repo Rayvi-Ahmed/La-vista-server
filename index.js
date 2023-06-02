6e0ac14f787e7c0d9d470f9e21193be57167cf36
Misbah Uddin <rayvi73@gmail.com>
2023-06-02 12:53:53 +0600

    user API created

--- index.js
+++ index.js
@@ -30,21 +30,38 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-
+        const usersCollection = client.db('VistaDb').collection('users')
         const menuCollection = client.db('VistaDb').collection("Menu")
         const reviewCollection = client.db('VistaDb').collection("Review")
         const cartsCollection = client.db('VistaDb').collection("Carts")
 
+
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            console.log(user)
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query)
+            console.log(existingUser)
+            if (existingUser) {
+                return res.send({ message: "Already exist this user" })
+            }
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
+        // Menu API
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray()
             res.send(result)
         })
 
+
+        // Review API 
         app.get('/review', async (req, res) => {
             const result = await reviewCollection.find().toArray()
             res.send(result)
         })
-
+        // Cart API 
         app.get('/carts', async (req, res) => {
             const email = req.query.email
             if (!email) {
@@ -69,6 +86,11 @@ async function run() {
             const result = await cartsCollection.deleteOne(query)
             res.send(result)
         })
+
+
+
+
+        // user API 
 
         // Send a ping to confirm a successful connection
 
