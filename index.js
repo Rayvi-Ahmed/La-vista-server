ea554ea7bd13b0e5e0a60f5fd85f5c33e5b66c13
Misbah Uddin <rayvi73@gmail.com>
2023-06-04 17:08:31 +0600

    Axios secure admin panel add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require("express")
+const express = require('express')
 const app = express()
+const jwt = require('jsonwebtoken')
 const cors = require('cors')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -10,9 +11,27 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+const verifyJwt = (req, res, next) => {
+    const authorization = req.headers.authorized;
+    if (!authorization) {
+        return res.status(401).send({ error: true, massage: 'Unauthorised user !' })
+    }
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, massage: 'Unauthorized user' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 app.get('/', (req, res) => {
     res.send('lavista spainish resrurent')
 })
+
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.poxvpxp.mongodb.net/?retryWrites=true&w=majority`;
@@ -41,6 +60,12 @@ async function run() {
             res.send(result)
         })
 
+        app.post('/jwt', (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body
             console.log(user)
@@ -52,6 +77,19 @@ async function run() {
             }
             const result = await usersCollection.insertOne(user)
             res.send(result)
+        })
+        app.get('/users/admin/:email', verifyJwt, async (req, res) => {
+            const email = req.params.email
+
+            if (req.decoded.email !== email) {
+                return res.send({ admin: false })
+            }
+
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            const result = { admin: user?.role === 'admin' }
+            res.send(result)
+
         })
 
         app.patch('/users/admin/:id', async (req, res) => {
@@ -69,6 +107,13 @@ async function run() {
 
         })
 
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await usersCollection.deleteOne(query)
+            res.send(result)
+        })
+
         // Menu API
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray()
@@ -82,12 +127,17 @@ async function run() {
             res.send(result)
         })
         // Cart API 
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJwt, async (req, res) => {
             const email = req.query.email
             if (!email) {
 
                 res.send([])
             }
+            const decodedEmail = req.decoded.email
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, massage: 'Access denied Unauthorized user' })
+            }
+
             const query = { email: email }
             const result = await cartsCollection.find(query).toArray()
             res.send(result)
@@ -106,7 +156,6 @@ async function run() {
             const result = await cartsCollection.deleteOne(query)
             res.send(result)
         })
-
 
 
 
